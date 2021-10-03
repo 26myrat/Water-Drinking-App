67cf9200f3d7c1e47860cd0cccacba0486adda25
26myrat <71564716+26myrat@users.noreply.github.com>
2021-10-03 10:28:12 -0700

    Add files via upload

---
+++ Screens/Medicine.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+export default class MedicineScreen extends React.Component{
+  render(){
+    return(
+      <View style= {{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Medicine Screen</Text>
+      </View>
+    )
+  }
+}
