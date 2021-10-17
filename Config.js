76e1c5c43c665374849c64ace12b2625f2a30088
26myrat <71564716+26myrat@users.noreply.github.com>
2021-10-17 09:13:33 -0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyAEaWEdLEEhiF9Jy9GkLIbWFFteycCYSuc",
+    authDomain: "water-drinker-app.firebaseapp.com",
+    projectId: "water-drinker-app",
+    storageBucket: "water-drinker-app.appspot.com",
+    messagingSenderId: "599277835873",
+    appId: "1:599277835873:web:4ed71493182fee38a4fc20"
+};
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase.firestore();
